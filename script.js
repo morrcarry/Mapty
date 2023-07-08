71bfe0487084b61c19513d163bdece6aafa60309
Mudit Singh <83901280+morrcarry@users.noreply.github.com>
2023-07-08 13:02:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,8 +115,7 @@ class App {
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+       attribution: '',
     }).addTo(this.#map);
 
     // Handling clicks on map
